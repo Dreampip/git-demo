aef1cd8aa52501f63ec6ee57033a81f097150bc7
Dream <1427863643@qq.com>
2022-04-24 21:43:45 +0800

    pull 测试2

--- src/main/java/com/pip/app/GitTest.java
+++ src/main/java/com/pip/app/GitTest.java
@@ -15,5 +15,21 @@ public class GitTest {
         System.out.println("hot fix 测试冲突");
         System.out.println("push 测试");
         System.out.println("pull 测试");
+
+        GitTest gitTest = new GitTest();//使用对象的非静态方法需要创建对象
+        int a = 5;
+        int b = 10;
+        gitTest.sum(a,b);// 通过对象名去调用求和的方法
+        int a2 = 23;
+        int b2 = 12;
+        gitTest.sum(a2,b2);// 通过对象名去调用求和的方法
+        int a3 = 53;
+        int b3 = 10;
+        gitTest.sum(a3,b3);// 通过对象名去调用求和的方法
+    }
+
+    public void sum(int x,int y){
+        int z = x + y;
+        System.out.println("z = " + z);
     }
 }
