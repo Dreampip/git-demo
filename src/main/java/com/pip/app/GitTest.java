64b0f36dcbac5443dc3682316bfa40ef936a9b8e
Dream <1427863643@qq.com>
2022-04-24 21:06:44 +0800

    master 1

--- src/main/java/com/pip/app/GitTest.java
+++ src/main/java/com/pip/app/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello,Git2!");
         System.out.println("hello,Git3!");
         System.out.println("hello,Git4!-hot fix");
+        System.out.println("master 测试合并冲突");
     }
 }
