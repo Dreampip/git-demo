25fddd8cc59d8a27803ead07ac9f54d444eb07e1
Dream <1427863643@qq.com>
2022-04-24 20:52:59 +0800

    变更4

---
+++ src/main/java/com/pip/app/GitTest2.java
@@ -0,0 +1,14 @@
+package com.pip.app;
+
+/**
+ * @Author: King
+ * @Date: 2022/4/24
+ * @Description: TODO 一句话描述该类的功能
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("hello,Git1");
+        System.out.println("hello,Git2");
+        System.out.println("hello,Git3");
+    }
+}
